fabf1ce539319d74fd79787f56c44c2d66849ca1
Oblakulov Mukhammadamin <moblakulov5@gmail.com>
2022-08-16 21:25:58 -0700

    started tracking .idea folder by committing it

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,11 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("HelloTest");
+
+        // work 1
+
+        // work 2
+
+        // work 3
     }
 }
